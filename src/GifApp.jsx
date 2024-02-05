a027e1c55d1fbfac34c5be622da4d8d8eac7c4b3
hcastill <herly.castillo_castillo.ext@nokia.com>
2024-02-05 10:53:38 -0500

    Proyecto inicial

---
+++ src/GifApp.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import {Button} from "react-bootstrap";
+
+export default function GifApp(){
+    
+    return (
+    <>
+        <h1>Hola Gif App</h1>
+        
+    </>
+    )
+}
